88e3ed13ec1b1aca28d2eb392433f16fb21f22b6
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-06-26 16:14:49 +0200

    chore: clean up
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- DesignToolbox/DesignToolboxUITests/Components/Switches/SwitchUITests.swift
+++ DesignToolbox/DesignToolboxUITests/Components/Switches/SwitchUITests.swift
@@ -25,11 +25,21 @@ final class SwitchUITests: AppTestCase {
 
         let app = launchApp()
         goToComponentsSheet(app)
+        swipeFromDownToUp(app)
+        swipeFromDownToUp(app)
         tapButton(withWording: "app_components_switch_label", app)
         tapButton(withWording: "app_components_switch_label", app)
 
         let `switch` = otherElements(withA11yIdentifier: A11YIdentifiers.componentSwitch, app).firstMatch
         XCTAssertTrue(`switch`.exists)
+        check(value: "core_common_selected_a11y", ofElementWithIdentifier: A11YIdentifiers.componentSwitch, app)
+
+        // WHEN
+
+        `switch`.tap()
+
+        // THEN
+
         check(value: "core_common_unselected_a11y", ofElementWithIdentifier: A11YIdentifiers.componentSwitch, app)
 
         // WHEN
