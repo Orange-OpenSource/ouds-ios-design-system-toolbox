9a06523d2d93d5a46e5f51c39e3592a2ea733dd0
Ludovic PINEL <ludovic.pinel@orange.com>
2025-06-27 14:33:21 +0200

    feat: Add SuggestionChip demo

---
+++ DesignToolbox/DesignToolbox/Pages/Components/Chips/Suggestion/SuggestionChipConfigurationView.swift
@@ -0,0 +1,125 @@
+//
+// Software Name: OUDS iOS
+// SPDX-FileCopyrightText: Copyright (c) Orange SA
+// SPDX-License-Identifier: MIT
+//
+// This software is distributed under the MIT license,
+// the text of which is available at https://opensource.org/license/MIT/
+// or see the "LICENSE" file for more details.
+//
+// Authors: See CONTRIBUTORS.txt
+// Software description: A SwiftUI components library with code examples for Orange Unified Design System
+//
+
+import OUDSComponents
+import SwiftUI
+
+// MARK: - SuggestionChip Configuration Model
+
+/// The model shared between `SuggestionChipPageConfiguration` view and `SuggestionChipPageComponent` view.
+final class SuggestionChipConfigurationModel: ComponentConfiguration {
+
+    // MARK: Published properties
+
+    @Published var enabled: Bool {
+        didSet { updateCode() }
+    }
+
+    @Published var text: String
+
+    @Published var layout: ChipLayout {
+        didSet { updateCode() }
+    }
+
+    // MARK: Initializer
+
+    override init() {
+        enabled = true
+        text = "app_components_chip_suggestionChip_suggestionChip_label".localized(with: 1)
+        layout = .textOnly
+    }
+
+    deinit {}
+
+    // MARK: Component Configuration
+
+    private var disableCode: String {
+        !enabled ? ".disabled(true)" : ""
+    }
+
+    override func updateCode() {
+        switch layout {
+        case .textOnly:
+            code =
+                """
+                OUDSChip(text: \"Chip\") {}
+                \(disableCode)
+                """
+        case .iconOnly:
+            code =
+                """
+                OUDSChip(icon: Image(\"ic_heart\")) {}
+                \(disableCode)
+                """
+        case .iconAndText:
+            code =
+                """
+                OUDSChip(icon: Image(\"ic_heart\", text: \"Chip\")) {}
+                \(disableCode)
+                """
+        }
+    }
+}
+
+// MARK: - Chip Layout
+
+enum ChipLayout: CaseIterable, CustomStringConvertible {
+    case textOnly
+    case iconAndText
+    case iconOnly
+
+    var description: String {
+        switch self {
+        case .textOnly:
+            "app_components_common_textOnlyLayout_label"
+        case .iconAndText:
+            "app_components_common_iconAndTextLayout_label"
+        case .iconOnly:
+            "app_components_common_iconOnlyLayout_label"
+        }
+    }
+
+    var id: String { description }
+}
+
+// MARK: - SuggestionChip Configuration View
+
+struct SuggestionChipConfigurationView: View {
+
+    @StateObject var configurationModel: SuggestionChipConfigurationModel
+
+    @Environment(\.theme) private var theme
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: theme.spaces.spaceFixedMedium) {
+            VStack(alignment: .leading, spacing: theme.spaces.spaceFixedNone) {
+                OUDSSwitchItem("app_common_enabled_label", isOn: $configurationModel.enabled)
+            }
+
+            DesignToolboxChoicePicker(title: "app_components_common_layout_label",
+                                      selection: $configurationModel.layout,
+                                      style: .segmented)
+            {
+                ForEach(ChipLayout.allCases, id: \.id) { layout in
+                    Text(LocalizedStringKey(layout.description)).tag(layout)
+                }
+            }
+
+            if configurationModel.layout == .iconAndText || configurationModel.layout == .textOnly {
+                DesignToolboxEditContentDisclosure {
+                    DesignToolboxTextField(text: $configurationModel.text)
+                }
+            }
+        }
+    }
+}
