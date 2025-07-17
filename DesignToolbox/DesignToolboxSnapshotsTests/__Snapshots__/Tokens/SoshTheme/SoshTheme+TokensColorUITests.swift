41a6ed28424183c048a1442b462fe6320219aa9a
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-07-17 16:43:06 +0200

    refactor= split color charts semantic tokens for Orange theme to dedicated optional provider (Orange-OpenSource/ouds-ios#856) (#39)
    Related to Orange-OpenSource/ouds-ios#856
    
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- DesignToolbox/DesignToolboxSnapshotsTests/__Snapshots__/Tokens/SoshTheme/SoshTheme+TokensColorUITests.swift
+++ DesignToolbox/DesignToolboxSnapshotsTests/__Snapshots__/Tokens/SoshTheme/SoshTheme+TokensColorUITests.swift
@@ -14,6 +14,7 @@
 import OUDS
 import OUDSThemesSosh
 import SwiftUI
+import XCTest
 
 // swiftlint:disable required_deinit
 
@@ -37,7 +38,11 @@ final class SoshThemeTokensColorUITests: TokensColorUITestsTestCase {
         testAlwaysColors(theme: theme, interfaceStyle: interfaceStyle)
         testBackgroundColors(theme: theme, interfaceStyle: interfaceStyle)
         testBorderColors(theme: theme, interfaceStyle: interfaceStyle)
-        testChartColors(theme: theme, interfaceStyle: interfaceStyle)
+        // Sosh theme does not have yet color charts branded for it
+        // If the provider is defined that's unexpected, it should be nil
+        if theme.colorCharts != nil {
+            XCTFail("The Sosh theme does not have yet color charts, that's unexpected")
+        }
         testContentColors(theme: theme, interfaceStyle: interfaceStyle)
         testDecorativeColors(theme: theme, interfaceStyle: interfaceStyle)
         testOpacityColors(theme: theme, interfaceStyle: interfaceStyle)
@@ -55,15 +60,19 @@ final class SoshThemeTokensColorUITests: TokensColorUITestsTestCase {
         testAlwaysColors(theme: theme, interfaceStyle: interfaceStyle)
         testBackgroundColors(theme: theme, interfaceStyle: interfaceStyle)
         testBorderColors(theme: theme, interfaceStyle: interfaceStyle)
-        testChartColors(theme: theme, interfaceStyle: interfaceStyle)
+        // Sosh theme does not have yet color charts branded for it
+        // If the provider is defined that's unexpected, it should be nil
+        if theme.colorCharts != nil {
+            XCTFail("The Sosh theme does not have yet color charts, that's unexpected")
+        }
         testContentColors(theme: theme, interfaceStyle: interfaceStyle)
         testDecorativeColors(theme: theme, interfaceStyle: interfaceStyle)
         testOpacityColors(theme: theme, interfaceStyle: interfaceStyle)
         testOverlayColors(theme: theme, interfaceStyle: interfaceStyle)
         /*
          There are some failures with two tokens, even if the snapshots are fresh:
-         - Soth Theme, dark mode --> colorRepositoryOpacityBlackHighest
-         - Soth Theme, dark mode --> colorSurfaceStatusNeutralEmphasized
+         - Sosh Theme, dark mode --> colorRepositoryOpacityBlackHighest
+         - Sosh Theme, dark mode --> colorSurfaceStatusNeutralEmphasized
          */
         testRepositoryColors(theme: theme, interfaceStyle: interfaceStyle, precision: 0.94, perceptualPrecision: 0.96)
         testSurfaceColors(theme: theme, interfaceStyle: interfaceStyle, precision: 0.94, perceptualPrecision: 0.96)
