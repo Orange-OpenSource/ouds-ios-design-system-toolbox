30cfa65bd96c4fcce7bba167dcd297ef33d8279c
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-07-24 18:37:50 +0200

    feat: add `Wireframe` theme (Orange-OpenSource/ouds-ios#669) (#44)
    Related to Orange-OpenSource/ouds-ios#669
    
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

---
+++ DesignToolbox/DesignToolboxSnapshotsTests/__Snapshots__/Components/OrangeBusinessToolsTheme/OrangeBusinessToolsTheme+DividerUITests.swift
@@ -0,0 +1,46 @@
+//
+// Software Name: OUDS iOS
+// SPDX-FileCopyrightText: Copyright (c) Orange SA
+// SPDX-License-Identifier: MIT
+//
+// This software is distributed under the MIT license,
+// the text of which is available at https://opensource.org/license/MIT/
+// or see the "LICENSE" file for more details.
+//
+// Authors: See CONTRIBUTORS.txt
+// Software description: A SwiftUI components library with code examples for Orange Unified Design System
+//
+
+import OUDS
+import OUDSThemesOrangeBusinessTools
+import SwiftUI
+
+// swiftlint:disable required_deinit
+
+// MARK: - Test Cases
+
+/// Tests the UI rendering of the `OUDSHorizontalDivider` and `OUDSVerticalDivider` for each parameter  for `OrangeBusinessToolsTheme`.
+final class OrangeBusinessToolsThemeDividerUITests: DividerUITestsTestCase {
+
+    // swiftlint:disable implicitly_unwrapped_optional
+    private var theme: OUDSTheme!
+    // swiftlint:enable implicitly_unwrapped_optional
+
+    override func setUp() {
+        theme = OrangeBusinessToolsTheme()
+    }
+
+    /// Tests all divider configuration in the `OrangeBusinessToolsTheme` with the `light` color schemes.
+    @MainActor func testAllDividerOrangeBusinessToolsThemeLight() {
+        let interfaceStyle = UIUserInterfaceStyle.light
+        testAllDividers(theme: theme, interfaceStyle: interfaceStyle)
+    }
+
+    /// Tests all buttons configuration in the `OrangeBusinessToolsTheme` with the `dark` color schemes.
+    @MainActor func testAllDividerOrangeBusinessToolsThemeDark() {
+        let interfaceStyle = UIUserInterfaceStyle.dark
+        testAllDividers(theme: theme, interfaceStyle: interfaceStyle)
+    }
+}
+
+// swiftlint:enable required_deinit
---
+++ DesignToolbox/DesignToolboxSnapshotsTests/__Snapshots__/Components/OrangeInverseTheme/OrangeInverseTheme+CheckboxUITests.swift
@@ -0,0 +1,48 @@
+//
+// Software Name: OUDS iOS
+// SPDX-FileCopyrightText: Copyright (c) Orange SA
+// SPDX-License-Identifier: MIT
+//
+// This software is distributed under the MIT license,
+// the text of which is available at https://opensource.org/license/MIT/
+// or see the "LICENSE" file for more details.
+//
+// Authors: See CONTRIBUTORS.txt
+// Software description: A SwiftUI components library with code examples for Orange Unified Design System
+//
+
+import OUDS
+import OUDSThemesOrangeInverse
+import SwiftUI
+
+// swiftlint:disable required_deinit
+
+// MARK: - Test Cases
+
+/// Tests the UI rendering of the `OUDSCheckboxIndeterminate` and `OUDSCheckboxItemIndeterminate` for each parameter.
+/// `OUDSCheckbox` and `OUDSCheckboxItem` won't be tested as, until today, are based on the same layouts and look and feels, and the only difference
+/// is in the management of the states. Uses `OrangeInverseTheme`.
+final class OrangeInverseThemeCheckboxUITests: CheckboxUITestsTestCase {
+
+    // swiftlint:disable implicitly_unwrapped_optional
+    private var theme: OUDSTheme!
+    // swiftlint:enable implicitly_unwrapped_optional
+
+    override func setUp() {
+        theme = OrangeInverseTheme()
+    }
+
+    /// Tests all buttons configuration in the `OrangeInverseTheme` with the `light` color schemes.
+    @MainActor func testAllCheckboxesOrangeInverseThemeLight() {
+        let interfaceStyle = UIUserInterfaceStyle.light
+        testAllCheckboxes(theme: theme, interfaceStyle: interfaceStyle)
+    }
+
+    /// Tests all buttons configuration in the `OrangeInverseTheme` with the `dark` color schemes.
+    @MainActor func testAllCheckboxesOrangeInverseThemeDark() {
+        let interfaceStyle = UIUserInterfaceStyle.dark
+        testAllCheckboxes(theme: theme, interfaceStyle: interfaceStyle)
+    }
+}
+
+// swiftlint:enable required_deinit
