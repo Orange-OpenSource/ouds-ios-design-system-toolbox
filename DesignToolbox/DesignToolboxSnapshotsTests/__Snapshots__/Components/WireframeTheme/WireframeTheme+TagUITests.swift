6fc7584be48df0d6d11afc419974083b29db19f3
LudovicPinel <ludovic.pinel@orange.com>
2025-08-20 16:06:12 +0200

    feat: add `tag` component (Orange-OpenSource/ouds-ios#408) (#49)
    Add OUDSTag component and tests
    
    Reviewed-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
    Co-authored-by: Ludovic Pinel <ludovic.pinel@orange.com>
    Co-authored-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

---
+++ DesignToolbox/DesignToolboxSnapshotsTests/__Snapshots__/Components/WireframeTheme/WireframeTheme+TagUITests.swift
@@ -0,0 +1,44 @@
+//
+// Software Name: OUDS iOS
+// SPDX-FileCopyrightText: Copyright (c) Orange SA
+// SPDX-License-Identifier: MIT
+//
+// This software is distributed under the MIT license,
+// the text of which is available at https://opensource.org/license/MIT/
+// or see the "LICENSE" file for more details.
+//
+// Authors: See CONTRIBUTORS.txt
+// Software description: A SwiftUI components library with code examples for Orange Unified Design System
+//
+
+import OUDS
+import OUDSThemesWireframe
+import SwiftUI
+
+// swiftlint:disable required_deinit
+
+/// Tests the UI rendering of the `OUDSTag` for each parameter with `WireframeTheme`.
+final class WireframeThemeTagUITests: TagUITestsTestCase {
+
+    // swiftlint:disable implicitly_unwrapped_optional
+    private var theme: OUDSTheme!
+    // swiftlint:enable implicitly_unwrapped_optional
+
+    override func setUp() {
+        theme = WireframeTheme()
+    }
+
+    /// Tests all tags configuration in the `Wireframe` with the `light` color scheme.
+    @MainActor func testAllTagsWireframeThemeLight() {
+        let interfaceStyle = UIUserInterfaceStyle.light
+        testAllTags(theme: theme, interfaceStyle: interfaceStyle)
+    }
+
+    /// Tests all tags configuration in the `Wireframe` with the `dark` color scheme.
+    @MainActor func testAllTagsWireframeThemeDark() {
+        let interfaceStyle = UIUserInterfaceStyle.dark
+        testAllTags(theme: theme, interfaceStyle: interfaceStyle)
+    }
+}
+
+// swiftlint:enable required_deinit
