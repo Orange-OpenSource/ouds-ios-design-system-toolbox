22e4e6072e07e6e3bacd569b2ef08e64f40bf9c3
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-10-28 15:57:26 +0100

    refactor: semantic tokens of `color chart` (Orange-OpenSource/ouds-ios#1030)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- DesignToolbox/DesignToolbox/Pages/Tokens/Color/NamedColor/NamedColor+Chart.swift
+++ DesignToolbox/DesignToolbox/Pages/Tokens/Color/NamedColor/NamedColor+Chart.swift
@@ -17,68 +17,68 @@ import OUDSTokensSemantic
 extension NamedColor {
 
     enum Chart: String, CaseIterable {
-        case chartBorder
-        case chartBorderContrast
-        case chartCategoricalTier1
-        case chartCategoricalTier2
-        case chartCategoricalTier3
-        case chartCategoricalTier4
-        case chartCategoricalTier5
-        case chartCategoricalTier6
-        case chartCategoricalTier7
-        case chartCategoricalTier8
-        case chartCategoricalTier9
-        case chartCategoricalTier10
-        case chartFunctionalInfo
-        case chartFunctionalNegative
-        case chartFunctionalPositive
-        case chartFunctionalWarning
-        case chartGridlines
-        case chartHighlight
-        case chartNeutral
+        case border
+        case borderContrast
+        case categoricalTier1
+        case categoricalTier2
+        case categoricalTier3
+        case categoricalTier4
+        case categoricalTier5
+        case categoricalTier6
+        case categoricalTier7
+        case categoricalTier8
+        case categoricalTier9
+        case categoricalTier10
+        case functionalInfo
+        case functionalNegative
+        case functionalPositive
+        case functionalWarning
+        case gridlines
+        case highlight
+        case neutral
 
         // We can bang! theme.charts here because controls on the existence of theme.charts have been previosuly done
         // swiftlint:disable force_unwrapping
         func token(from theme: OUDSTheme) -> MultipleColorSemanticTokens {
             switch self {
-            case .chartBorder:
-                theme.charts!.chartBorder
-            case .chartBorderContrast:
-                theme.charts!.chartBorderContrast
-            case .chartCategoricalTier1:
-                theme.charts!.chartCategoricalTier1
-            case .chartCategoricalTier2:
-                theme.charts!.chartCategoricalTier2
-            case .chartCategoricalTier3:
-                theme.charts!.chartCategoricalTier3
-            case .chartCategoricalTier4:
-                theme.charts!.chartCategoricalTier4
-            case .chartCategoricalTier5:
-                theme.charts!.chartCategoricalTier5
-            case .chartCategoricalTier6:
-                theme.charts!.chartCategoricalTier6
-            case .chartCategoricalTier7:
-                theme.charts!.chartCategoricalTier7
-            case .chartCategoricalTier8:
-                theme.charts!.chartCategoricalTier8
-            case .chartCategoricalTier9:
-                theme.charts!.chartCategoricalTier9
-            case .chartCategoricalTier10:
-                theme.charts!.chartCategoricalTier10
-            case .chartFunctionalInfo:
-                theme.charts!.chartFunctionalInfo
-            case .chartFunctionalNegative:
-                theme.charts!.chartFunctionalNegative
-            case .chartFunctionalPositive:
-                theme.charts!.chartFunctionalPositive
-            case .chartFunctionalWarning:
-                theme.charts!.chartFunctionalWarning
-            case .chartGridlines:
-                theme.charts!.chartGridlines
-            case .chartHighlight:
-                theme.charts!.chartHighlight
-            case .chartNeutral:
-                theme.charts!.chartNeutral
+            case .border:
+                theme.charts!.border
+            case .borderContrast:
+                theme.charts!.borderContrast
+            case .categoricalTier1:
+                theme.charts!.categoricalTier1
+            case .categoricalTier2:
+                theme.charts!.categoricalTier2
+            case .categoricalTier3:
+                theme.charts!.categoricalTier3
+            case .categoricalTier4:
+                theme.charts!.categoricalTier4
+            case .categoricalTier5:
+                theme.charts!.categoricalTier5
+            case .categoricalTier6:
+                theme.charts!.categoricalTier6
+            case .categoricalTier7:
+                theme.charts!.categoricalTier7
+            case .categoricalTier8:
+                theme.charts!.categoricalTier8
+            case .categoricalTier9:
+                theme.charts!.categoricalTier9
+            case .categoricalTier10:
+                theme.charts!.categoricalTier10
+            case .functionalInfo:
+                theme.charts!.functionalInfo
+            case .functionalNegative:
+                theme.charts!.functionalNegative
+            case .functionalPositive:
+                theme.charts!.functionalPositive
+            case .functionalWarning:
+                theme.charts!.functionalWarning
+            case .gridlines:
+                theme.charts!.gridlines
+            case .highlight:
+                theme.charts!.highlight
+            case .neutral:
+                theme.charts!.neutral
             }
         }
         // swiftlint:enable force_unwrapping
