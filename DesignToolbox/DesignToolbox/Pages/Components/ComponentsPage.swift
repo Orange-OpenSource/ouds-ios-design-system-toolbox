5e2a0ee9815aebf9446f63792c11f587c0855511
Ludovic PINEL <ludovic.pinel@orange.com>
2026-02-06 20:53:20 +0100

    feat: first version without tokens

--- DesignToolbox/DesignToolbox/Pages/Components/ComponentsPage.swift
+++ DesignToolbox/DesignToolbox/Pages/Components/ComponentsPage.swift
@@ -16,6 +16,7 @@ import SwiftUI
 struct ComponentsPage: View {
 
     var componentElements: [DesignToolboxElement] = [
+        AlertMessageElement(),        
         BadgeElement(),
         BulletListElement(),
         ButtonElement(),
