3ab2604372a766be07a57809dd7d319ba877d659
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2026-03-17 20:52:51 +0100

    fix: add missing translations (Orange-OpenSource/ouds-ios#1325)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- DesignToolbox/DesignToolbox/Pages/Components/Layouts/ColoredSurface/ColoredSurfacePage.swift
+++ DesignToolbox/DesignToolbox/Pages/Components/Layouts/ColoredSurface/ColoredSurfacePage.swift
@@ -46,17 +46,16 @@ private struct ColoredSurfaceDemo: View {
                 Text(configurationModel.selectedColor.formattedName)
                     .oudsForegroundColor(theme.colors.contentDefault)
 
-                    OUDSButton(text: "app_components_button_tech".localized(),
-                               appearance: .default,
-                               style: .default) {}
+                OUDSButton(text: "app_components_button_tech".localized(),
+                           appearance: .default,
+                           style: .default) {}
 
-                    OUDSLink(text: "app_components_link_tech".localized(), indicator: .next) {}
-                }
-                .frame(maxWidth: .infinity, alignment: .center)
-                .padding(.all, theme.spaces.fixedMedium)
+                OUDSLink(text: "app_components_link_tech".localized(), indicator: .next) {}
             }
             .frame(maxWidth: .infinity, alignment: .center)
             .padding(.all, theme.spaces.fixedMedium)
         }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .padding(.all, theme.spaces.fixedMedium)
     }
 }
