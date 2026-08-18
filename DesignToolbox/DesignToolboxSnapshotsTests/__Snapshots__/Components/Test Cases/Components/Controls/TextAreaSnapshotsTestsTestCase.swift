6a5a41824d1f5fff2d67c53d1f26526bd0bad8f8
LudovicPinel <ludovic.pinel@orange.com>
2026-08-18 17:13:22 +0200

    refactor: update text area component to version 1.2.1 (Orange-OpenSource/ouds-ios#1527) (#279)
    Related to Orange-OpenSource/ouds-ios#1527
    
    Tested-by: Jérôme Regnier <jerome.regnier@orange.com>
    Reviewed-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- DesignToolbox/DesignToolboxSnapshotsTests/__Snapshots__/Components/Test Cases/Components/Controls/TextAreaSnapshotsTestsTestCase.swift
+++ DesignToolbox/DesignToolboxSnapshotsTests/__Snapshots__/Components/Test Cases/Components/Controls/TextAreaSnapshotsTestsTestCase.swift
@@ -38,7 +38,7 @@ open class TextAreaSnapshotsTestsTestCase: XCTestCase {
         for outlined in [true, false] {
             for constrainedMaxWidth in [true, false] {
                 // Drop the loading status still the progress indicator is done
-                for status in OUDSTextArea.Status.allCases where status != .loading {
+                for status in OUDSTextArea.Status.allCases {
                     testTextArea(theme: theme,
                                  interfaceStyle: interfaceStyle,
                                  testType: .styleAndStatus,
@@ -105,6 +105,8 @@ open class TextAreaSnapshotsTestsTestCase: XCTestCase {
 
 // swiftlint:enable required_deinit
 
+// MARK: - Test Text Area View
+
 struct TestTextAreaView: View {
 
     /// Two types of test
@@ -115,7 +117,7 @@ struct TestTextAreaView: View {
         case helpers
     }
 
-    // MARK: - Stored properties
+    // MARK: - Properties
 
     let type: TestType
     let outlined: Bool
@@ -207,7 +209,7 @@ struct TestTextAreaView: View {
 extension OUDSTextArea.Status: @retroactive CaseIterable, @retroactive Hashable, DesignToolboxEnumLocalizedRepresentable {
 
     public static let allCases: [OUDSTextArea.Status] =
-        [.enabled, .error(message: "app_components_textArea_errorDescription_label".localized()), .loading, .readOnly, .disabled]
+        [.enabled, .error(message: "app_components_textArea_errorDescription_label".localized()), .loading(progress: 0.63), .readOnly, .disabled]
 
     public var wordingKey: String {
         switch self {
