636fb80bc39494a72b7a6fe20b42b5458587e4a5
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2026-09-25 18:58:34 +0200

    chore: clean up
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- DesignToolbox/DesignToolbox/Pages/Utils/DesignToolboxEditContentDisclosure.swift
+++ DesignToolbox/DesignToolbox/Pages/Utils/DesignToolboxEditContentDisclosure.swift
@@ -31,8 +31,8 @@ struct DesignToolboxEditContentDisclosure<Content>: View where Content: View {
          @ViewBuilder content: @escaping () -> Content)
     {
         self.title = title.localized()
-        self.content = content
         self.isContentVisible = isContentVisible
+        self.content = content
     }
 
     // MARK: - Body
